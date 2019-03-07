a3c1627d930572a19d18ce0c78e5942265d61558
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2019-03-07 10:33:20 -0300

    Create plasma_baloo.sh

---
+++ manjaro/plasma_baloo.sh
@@ -0,0 +1,12 @@
+
+sudo rm /etc/xdg/autostart/baloo_file.desktop
+sudo touch /etc/xdg/autostart/baloo_file.desktop
+
+# Replate `~/.config/ballofilerc` for:
+```
+[Basic Settings]
+Indexing-Enabled=false
+
+[General]
+first run=false
+```
