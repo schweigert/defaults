2e80de9a171b77cb7cdec9be9f10107e89d84cdc
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2019-05-30 14:20:16 -0300

    Update golang version to 1.12

--- manjaro/golang.sh
+++ manjaro/golang.sh
@@ -2,5 +2,5 @@
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
 # Install golang
-gvm install go1.11 -B
-gvm use go1.11
+gvm install go1.12 -B
+gvm use go1.12
