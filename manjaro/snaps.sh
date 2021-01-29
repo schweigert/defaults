8c23b4b22660775718c99403e8a74652c5315180
Marlon Henry Schweigert <marlon.schweigert@krakenlab.io>
2021-01-29 12:11:27 -0300

    Update snaps.sh

--- manjaro/snaps.sh
+++ manjaro/snaps.sh
@@ -5,3 +5,6 @@ sudo snap install atom --classic
 sudo snap install skype --classic
 sudo snap install code --classic
 sudo snap install odrive --edge
+
+sudo snap install brave
+sudo snap connect brave:camera core:camera
